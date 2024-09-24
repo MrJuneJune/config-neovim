f42321c25c3da285cfe340e6f1b0222968dcce70
MrJuneJune <parkjune1995@gmail.com>
2024-09-24 10:24:24 -0400

    Updated init and start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
+# Install all dependency
+brew install neovim cmake make python gcc npm clangd ripgrep
+
+# Install packers
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-brew install neovim
+# Python linting
 pip install black
 
